9e0076bb503172c24d5b17ad620bc5e894db87b9
Hector <baseballhector@gmail.com>
2023-09-03 19:57:44 -0400

    Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="title-container">
+      <h1 className="title-text">Japanese Cuisine Memory Matching Game</h1>
+    </div>
+  );
+}
+
+export default Title;
