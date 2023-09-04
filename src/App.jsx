fa51e5a7a795bf486a4922fdac939009de02acc8
Hector <baseballhector@gmail.com>
2023-09-03 20:53:03 -0400

    Imported game component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Title from "./components/Title/Title";
-
 import "./App.css";
+
+import Title from "./components/Title/Title";
+import Game from "./components/Game/Game";
 
 function App() {
   return (
     <>
       <Title />
+      <Game />
     </>
   );
 }
