8b2432a6a7408b8bd443a0d1e9a747f6116d9f8f
Hector <baseballhector@gmail.com>
2023-09-04 19:57:58 -0400

    Storing food images data

---
+++ public/cardData.js
@@ -0,0 +1,50 @@
+export const cardData = [
+  {
+    key: 1,
+    name: "Takoyaki",
+  },
+  {
+    key: 2,
+    name: "Sushi",
+  },
+  {
+    key: 3,
+    name: "Ramen",
+  },
+  {
+    key: 4,
+    name: "Gyudon",
+  },
+  {
+    key: 5,
+    name: "Mitarashi Dango",
+  },
+  {
+    key: 6,
+    name: "Yakitori",
+  },
+  {
+    key: 7,
+    name: "Natto",
+  },
+  {
+    key: 8,
+    name: "Melon Pan",
+  },
+  {
+    key: 9,
+    name: "Karaage",
+  },
+  {
+    key: 10,
+    name: "Konpeito",
+  },
+  {
+    key: 11,
+    name: "Tonkatsu",
+  },
+  {
+    key: 12,
+    name: "Taiyaki",
+  },
+];
