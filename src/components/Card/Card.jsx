c5f616bfc1d21a9c6c90ca5283b7898e9741470e
Hector <baseballhector@gmail.com>
2023-09-05 20:34:40 -0400

    Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import "./Card.css";
+
+function Card(props) {
+  const id = props.id;
+  const name = props.name;
+
+  const baseImageUrl = "src/assets/cardImages/foodImage";
+  const imageExt = ".jpg";
+
+  const imageUrl = baseImageUrl + id + imageExt;
+
+  return (
+    <div className="card">
+      <div className="food-name">{name}</div>
+      <img className="food-image" src={imageUrl}></img>
+    </div>
+  );
+}
+
+export default Card;
