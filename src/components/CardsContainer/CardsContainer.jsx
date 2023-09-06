c7fcdf1a75d0bebabeaee5b8c28eeeb2e03ead63
Hector <baseballhector@gmail.com>
2023-09-06 18:00:45 -0400

    Randomizes the cards

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -2,10 +2,18 @@ import Card from "../Card/Card";
 
 import "./CardsContainer.css";
 
-import { cardData } from "../../assets/cardData";
+import { cardData } from "../../../public/assets/cardData";
+
+function randomizeCards() {
+  const randomCardData = cardData.sort(() => Math.random() - 0.5);
+
+  return randomCardData;
+}
 
 function CardsContainer({ handleCardClicked }) {
-  const childrenItems = cardData.map((food) => (
+  const randomCards = randomizeCards();
+
+  const childrenItems = randomCards.map((food) => (
     <Card
       key={food.id}
       id={food.id}
