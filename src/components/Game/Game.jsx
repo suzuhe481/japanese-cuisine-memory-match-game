162015ef23396aeb59c6b4b88193bbff506abbb9
Hector <baseballhector@gmail.com>
2023-09-06 17:08:53 -0400

    Function to handle card click

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,10 +4,20 @@ import CardsContainer from "../CardsContainer/CardsContainer";
 import "./Game.css";
 
 function Game() {
+  function handleCardClicked(e) {
+    console.log("card was clicked");
+    console.log(e.currentTarget);
+    console.log(e.currentTarget.value);
+    console.log(e.currentTarget.getAttribute("value"));
+
+    const cardId = e.currentTarget.getAttribute("value");
+    console.log(typeof cardId);
+  }
+
   return (
     <div className="game-container">
       <Score />
-      <CardsContainer />
+      <CardsContainer handleCardClicked={handleCardClicked} />
     </div>
   );
 }
