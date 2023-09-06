30e460f9c481b946adca7e92a1f288a380bc1a40
Hector <baseballhector@gmail.com>
2023-09-05 20:34:27 -0400

    Added cards container component

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,4 +1,5 @@
 import Score from "../Score/Score";
+import CardsContainer from "../CardsContainer/CardsContainer";
 
 import "./Game.css";
 
@@ -6,6 +7,7 @@ function Game() {
   return (
     <div className="game-container">
       <Score />
+      <CardsContainer />
     </div>
   );
 }
