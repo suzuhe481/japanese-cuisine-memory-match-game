c9acf411b1ba276d35994fbceb2924dffe1a68fc
Hector <baseballhector@gmail.com>
2023-09-07 19:24:17 -0400

    Displays a win message when conditions reached

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,10 +1,25 @@
 import "./Score.css";
 
 const totalCards = 12;
+const winningScore = 0;
+
+function winMessage() {
+  return <h1>You win!</h1>;
+}
 
 function Score({ cardsPicked, cardCorrect }) {
-  // Calculates score of remaining cards.
+  // Calculates score based on remaining cards needed.
   const score = totalCards - cardsPicked.length;
+
+  // Determines whether a win message should be created.
+  var winningMessage;
+  var gameWon = false;
+  if (score === winningScore) {
+    gameWon = true;
+    winningMessage = winMessage();
+  } else {
+    winningMessage = "";
+  }
 
   // Determines which icon should be displayed on screen.
   var correctIcon;
@@ -14,18 +29,35 @@ function Score({ cardsPicked, cardCorrect }) {
     incorrectIcon = "";
   } else {
     correctIcon = cardCorrect && (
-      <i className="correct-icon fa-solid fa-check fa-beat fa-2x"></i>
+      <i
+        className={`correct-icon fa-solid fa-check fa-beat fa-2x ${
+          gameWon ? "hidden" : ""
+        }`}
+      ></i>
     );
     incorrectIcon = !cardCorrect && (
-      <i className="incorrect-icon fa-solid fa-x fa-shake fa-2x"></i>
+      <i
+        className={`incorrect-icon fa-solid fa-x fa-shake fa-2x ${
+          gameWon ? "hidden" : ""
+        }`}
+      ></i>
     );
   }
 
   return (
     <div className="score-container">
-      <div id="correct-icon">{correctIcon}</div>
-      <div className="score">Cards Left: {score}</div>
-      <div id="incorrect-icon">{incorrectIcon}</div>
+      <div className={`win-message ${!gameWon ? "hidden" : ""}`}>
+        {winningMessage}
+      </div>
+      <div id="correct-icon" className={`${gameWon ? "hidden" : ""}`}>
+        {correctIcon}
+      </div>
+      <div className={`score ${gameWon ? "hidden" : ""}`}>
+        {!gameWon ? `Cards Left: ${score}` : ""}
+      </div>
+      <div id="incorrect-icon" className={`${gameWon ? "hidden" : ""}`}>
+        {incorrectIcon}
+      </div>
     </div>
   );
 }
