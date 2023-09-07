3d619821c3f129603130ef74a35bba96149e1f0a
Hector <baseballhector@gmail.com>
2023-09-07 13:53:02 -0400

    Moved title text to variable

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,12 +1,14 @@
 import "./Title.css";
 
 function Title() {
+  const titleText = "Memory Match Game - Japanese Cuisine";
+
   const rulesText =
     "Click each image only once. Make sure to remember them or you'll have to start over.";
 
   return (
     <div className="title-container">
-      <h1 className="title-text">Memory Match Game - Japanese Cuisine</h1>
+      <h1 className="title-text">{titleText}</h1>
       <p className="game-rules">{rulesText}</p>
     </div>
   );
