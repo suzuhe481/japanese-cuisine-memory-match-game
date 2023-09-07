1ecc39dfc92e7b717d482872bd6181678430a97a
Hector <baseballhector@gmail.com>
2023-09-07 13:52:14 -0400

    Added text with rules

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,9 +1,13 @@
 import "./Title.css";
 
 function Title() {
+  const rulesText =
+    "Click each image only once. Make sure to remember them or you'll have to start over.";
+
   return (
     <div className="title-container">
       <h1 className="title-text">Memory Match Game - Japanese Cuisine</h1>
+      <p className="game-rules">{rulesText}</p>
     </div>
   );
 }
