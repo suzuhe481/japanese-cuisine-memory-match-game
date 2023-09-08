9336237fb881710b336bfeb2e50ec12da0584a52
Hector <baseballhector@gmail.com>
2023-09-08 19:27:01 -0400

    Release 1

---
+++ dist/assets/cardData.js
@@ -0,0 +1,50 @@
+export const cardData = [
+  {
+    id: 1,
+    name: "Takoyaki",
+  },
+  {
+    id: 2,
+    name: "Sushi",
+  },
+  {
+    id: 3,
+    name: "Ramen",
+  },
+  {
+    id: 4,
+    name: "Gyudon",
+  },
+  {
+    id: 5,
+    name: "Mitarashi Dango",
+  },
+  {
+    id: 6,
+    name: "Yakitori",
+  },
+  {
+    id: 7,
+    name: "Natto",
+  },
+  {
+    id: 8,
+    name: "Melon Pan",
+  },
+  {
+    id: 9,
+    name: "Karaage",
+  },
+  {
+    id: 10,
+    name: "Konpeito",
+  },
+  {
+    id: 11,
+    name: "Tonkatsu",
+  },
+  {
+    id: 12,
+    name: "Taiyaki",
+  },
+];
