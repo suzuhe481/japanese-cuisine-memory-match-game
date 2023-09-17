a4c6dd49b4597065d496cd621d43e5ba4431fe4f
Hector <baseballhector@gmail.com>
2023-09-17 17:26:38 -0400

    useState to keep track of icon classes. useEffect to stop icon animation after 1 second

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import "./Score.css";
 
 const totalCards = 12;
@@ -8,6 +10,10 @@ function winMessage() {
 }
 
 function Score({ cardsPicked, cardCorrect }) {
+  const [correctIconClasses, setCorrectIconClasses] = useState("");
+  const [incorrectIconClasses, setIncorrectIconClasses] = useState("");
+  const [isAnimated, setIsAnimated] = useState(false);
+
   // Calculates score based on remaining cards needed.
   const score = totalCards - cardsPicked.length;
 
@@ -21,28 +27,30 @@ function Score({ cardsPicked, cardCorrect }) {
     winningMessage = "";
   }
 
-  // Determines which icon should be displayed on screen.
-  var correctIcon;
-  var incorrectIcon;
-  if (typeof cardCorrect === "undefined") {
-    correctIcon = "";
-    incorrectIcon = "";
-  } else {
-    correctIcon = cardCorrect && (
-      <i
-        className={`correct-icon fa-solid fa-check fa-beat fa-2x ${
-          gameWon ? "hidden" : ""
-        }`}
-      ></i>
-    );
-    incorrectIcon = !cardCorrect && (
-      <i
-        className={`incorrect-icon fa-solid fa-x fa-shake fa-2x ${
-          gameWon ? "hidden" : ""
-        }`}
-      ></i>
-    );
+  // Sets the appropriate classes for the correct and incorrect icons.
+  if (cardCorrect === true || cardCorrect === false) {
+    if (cardCorrect && correctIconClasses === "") {
+      setCorrectIconClasses(`correct-icon fa-solid fa-check fa-2x`);
+      setIncorrectIconClasses("");
+    } else if (!cardCorrect && incorrectIconClasses === "") {
+      setIncorrectIconClasses(`incorrect-icon fa-solid fa-x fa-2x`);
+      setCorrectIconClasses("");
+    }
   }
+
+  // Sets the isAnimated state to false after 1 second.
+  // This prevents the font awesome animation classes from being added to icons.
+  useEffect(() => {
+    setIsAnimated(true);
+
+    var timer = setTimeout(() => {
+      setIsAnimated(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cardsPicked]);
 
   return (
     <div className="score-container">
@@ -50,13 +58,17 @@ function Score({ cardsPicked, cardCorrect }) {
         {winningMessage}
       </div>
       <div id="correct-icon" className={`${gameWon ? "hidden" : ""}`}>
-        {correctIcon}
+        <i
+          className={`${correctIconClasses} ${isAnimated ? "fa-beat" : ""}`}
+        ></i>
       </div>
       <div className={`score ${gameWon ? "hidden" : ""}`}>
         {!gameWon ? `Cards Left: ${score}` : ""}
       </div>
       <div id="incorrect-icon" className={`${gameWon ? "hidden" : ""}`}>
-        {incorrectIcon}
+        <i
+          className={`${incorrectIconClasses} ${isAnimated ? "fa-shake" : ""}`}
+        ></i>
       </div>
     </div>
   );
